ea46601321694be20f657671bdd5832ba4833882
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2026-01-29 22:40:17 +0300

    I added a shared strings file

---
+++ lib/utils/app_strings.dart
@@ -0,0 +1,8 @@
+/// Central place for user-facing text.
+///
+/// Keeping strings in one place makes it easier to update wording later.
+class AppStrings {
+  AppStrings._();
+
+  static const String appName = 'Spotify Clone';
+}
